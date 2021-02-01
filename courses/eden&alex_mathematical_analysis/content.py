fa4fba1e3065d2a266347e271dc20b9aec910170
Maki-Lu <makimakima1231@gmail.com>
2021-01-31 20:41:44 -0500

    a

--- courses/eden&alex_mathematical_analysis/content.py
+++ courses/eden&alex_mathematical_analysis/content.py
@@ -6,19 +6,30 @@ author=""
 course_name=""
 l=[]
 s=""
+flag=-1
 for x in f:
     x=x.rstrip("\n")
     if len(x)!=0:
         if x[0] == "#":
             course_name=x
         else:
-            if "." not in x:
-                author=x
-            if  "." in x:
-                temp=x.split(".")
-                chapter_number=temp[0]
-                chapter_name=temp[1].strip()
-                s=s+"<li>\n"+"<li><a onclick=\"find_html_short(" + str(chapter_number) +")\">"+  x +"</a></li>\n"+"</li>\n"
+            if flag==1:
+                if "." not in x:
+                    author=x
+                else:
+                    temp=x.split(".")
+                    chapter_number=temp[0]
+                    chapter_name=temp[1].strip()
+                    s=s+"<li>\n"+"<li><a onclick=\"find_html_short(" + str(chapter_number) +")\">"+  x +"</a></li>\n"+"</li>\n"
+            if flag==2:
+                if "." not in x:
+                    author=x
+                else:
+                    print(x)
+            if x.strip()=="小课":
+                flag=1
+            if x.strip()=="大课":
+                flag=2
 
 f = open("content.txt", "w")
 f.write(s)
